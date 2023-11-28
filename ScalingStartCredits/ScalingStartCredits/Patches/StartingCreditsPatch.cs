4fa4367790a2cd87e22e73856849d3d1bec8f34a
Sunny <outofinkinc@gmail.com>
2023-11-28 14:54:39 -0500

    Initial commit

---
+++ ScalingStartCredits/ScalingStartCredits/Patches/StartingCreditsPatch.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using GameNetcodeStuff;
+using HarmonyLib;
+using BepInEx.Logging;
+using BepInEx;
+using BepInEx.Logging;
+using UnityEngine;
+
+namespace ScalingStartCredits.Patches
+{
+    [HarmonyPatch(typeof(StartMatchLever))]
+    internal class StartingCreditsPatch
+    {
+        
+        private static ManualLogSource mls = BepInEx.Logging.Logger.CreateLogSource(ScalingStartCreditsBase.modGUID);
+
+        private static int groupCreditIncreaseIncrement;
+        private static int playerThreshold;
+        private static int playerCount;
+        private static int days;
+
+        private static HUDManager hudManager;
+
+        [HarmonyPatch("StartGame")]
+        [HarmonyPrefix]
+        private static void StartMatchLeverPatch(ref StartOfRound ___playersManager)
+        {
+            groupCreditIncreaseIncrement = ScalingStartCreditsBase.Instance.configCreditIncrement.Value;
+            playerThreshold = ScalingStartCreditsBase.Instance.configPlayerCountThreshold.Value;
+            playerCount = ___playersManager.fullyLoadedPlayers.Count;
+            days = ___playersManager.daysPlayersSurvivedInARow;
+            bool conditionsMet = true;
+
+
+            mls.LogInfo($"Days: {days}");
+            mls.LogInfo($"Player Count: {playerCount}");
+
+            if ( days != 0)
+            {
+                mls.LogInfo($"No longer first day. Aborting");
+                conditionsMet = false;
+            }
+
+            if (playerCount <= playerThreshold && conditionsMet)
+            {
+                mls.LogInfo($"Player threshold not met. Aborting");
+                conditionsMet = false;
+            }
+
+            if (conditionsMet)
+            {
+                hudManager = (HUDManager)UnityEngine.Object.FindObjectOfType(typeof(HUDManager));
+                Terminal terminal = (Terminal)UnityEngine.Object.FindObjectOfType(typeof(Terminal));
+
+                int additionalPlayers = playerCount - playerThreshold;
+                int increaseInGroupCredits = additionalPlayers * groupCreditIncreaseIncrement;
+                int newGroupCredits = terminal.groupCredits + increaseInGroupCredits;
+                terminal.SyncGroupCreditsServerRpc(newGroupCredits, terminal.numberOfItemsInDropship);
+
+
+                mls.LogInfo($"Credits Added for {additionalPlayers} players above threshold of {playerThreshold}: {increaseInGroupCredits}");
+                mls.LogInfo($"Group Credits: {terminal.groupCredits}");
+
+                hudManager.AddTextToChatOnServer($"Credits Added for {additionalPlayers} players above threshold of {playerThreshold}: {increaseInGroupCredits}");
+                hudManager.AddTextToChatOnServer($"Group Credits: {terminal.groupCredits}");
+            }
+        }
+    }
+}
---
+++ ScalingStartCredits/ScalingStartCredits/Plugin.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+using HarmonyLib;
+using UnityEngine;
+
+namespace ScalingStartCredits
+{
+    [BepInPlugin(modGUID, modName, modVersion)]
+    public class ScalingStartCreditsBase : BaseUnityPlugin
+    {
+        public const string modGUID = "sunnobunno.scalingstartcredits";
+        public const string modName = "Scaling Start Credits";
+        public const string modVersion = "1.0.0";
+
+        private readonly Harmony harmony = new Harmony(modGUID);
+
+        public static ScalingStartCreditsBase? Instance;
+
+        internal ManualLogSource? mls;
+
+        public ConfigEntry<int> configCreditIncrement;
+        public ConfigEntry<int> configPlayerCountThreshold;
+
+        void Awake()
+        {
+            if (Instance == null)
+            {
+                Instance = this;
+            }
+
+            BindConfiguration();
+
+            mls = BepInEx.Logging.Logger.CreateLogSource(modGUID);
+            mls.LogInfo($"{modGUID} is loading.");
+
+            harmony.PatchAll();
+            mls.LogInfo($"{modGUID} is loaded.");
+        }
+
+        private void BindConfiguration()
+        {
+            configCreditIncrement = Config.Bind("General",
+                "Credit Increment",
+                15,
+                "The amount of credits per player added to the starting group credits");
+
+            configPlayerCountThreshold = Config.Bind("General",
+                "Player Threshold",
+                4,
+                "The number of players required in the lobby before credits are added per new player.");
+        }
+    }
+}
